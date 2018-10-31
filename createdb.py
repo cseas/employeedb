b130a59b04a5e0a524e3666c99af7d64f1c33827
cseas <contact.abhijeetsingh@gmail.com>
2018-10-31 12:28:18 +0530

    DB setup

--- createdb.py
+++ createdb.py
@@ -5,3 +5,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 Base = declarative_base()
+
+class Employee(Base):
+    __tablename__ = 'employee'
+    name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+
+class Address(Base):
+    __tablename__ = 'address'
+
+    street = Column(String(80), nullable=False)
+    zip = Column(String(5), nullable=False)
+    id = Column(Integer, primary_key=True)
+    employee_id = Column(Integer, ForeignKey('employee.id'))
+    employee = relationship(Employee)
+
+engine = create_engine('sqlite:///employeeData.db')
+Base.metadata.create_all(engine)
